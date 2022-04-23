d69ef622b4a55c85483110946d546ad47a9a3ee8
Kushagra Sharma <skushagra2003@gmail.com>
2022-04-23 13:28:03 +0530

    added admin auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,19 @@ import Home from './pages/home'
 import AddTip from './pages/addTip'
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { TransactionsProvider } from './context/TransactionContext'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <TransactionsProvider>
       <Routes>
         <Route path="/addTip" element={<AddTip/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/admin" element={<Admin/>}/>
         <Route path="/" exact element={<Index />}/>
       </Routes>
+    </TransactionsProvider>
     </BrowserRouter>
   )
 }
--- client/src/pages/admin.jsx
+++ client/src/pages/admin.jsx
@@ -4,7 +4,44 @@ const Admin = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-5">
       <main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
-          Admin
+          Admin Dashboard
+          <div className='flex w-full my-5 flex-col items-start'>
+            <h3 className='text-lg font-inter mb-5'>Past Tip-Off</h3>
+
+            <table className='border w-full text-left font-inter'>
+              <thead>
+                <tr className='border'>
+                  <th className='text-gray-900 px-6 py-4 w-1/5'>Name</th>
+                  <th className='text-gray-900 px-6 py-4 w-3/5'>Scenario</th>
+                  <th className='text-gray-900 px-6 py-4 w-1/5'>Reward</th>
+                </tr>
+              </thead>
+
+              <tbody className='table-body'>
+                <tr>
+                  <td className='px-6 py-4 w-1/5'>Something</td>
+                  <td className='text-gray-900 px-6 py-4 w-1/5'>Sooooo this soo that soo thiss</td>
+                  <td className='text-gray-900 px-6 py-4 w-1/5'>0.003 ETH</td>
+                </tr>
+                <tr>
+                  <td className='px-6 py-4 w-1/5'>Something</td>
+                  <td className='px-6 py-4 w-1/5'>Sooooo this soo that soo thiss</td>
+                  <td className='px-6 py-4 w-1/5'>0.003 ETH</td>
+                </tr>
+                <tr>
+                  <td className='px-6 py-4 w-1/5'>Something</td>
+                  <td className='px-6 py-4 w-1/5'>Sooooo this soo that soo thiss</td>
+                  <td className='px-6 py-4 w-1/5'>0.003 ETH</td>
+                </tr>
+                <tr>
+                  <td className='px-6 py-4 w-1/5'>Something</td>
+                  <td className='px-6 py-4 w-1/5'>Sooooo this soo that soo thiss</td>
+                  <td className='px-6 py-4 w-1/5'>0.003 ETH</td>
+                </tr>
+              </tbody>
+              
+            </table>
+          </div>
       </main>
       <Footer />
     </div>
