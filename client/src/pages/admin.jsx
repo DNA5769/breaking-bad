f38533b505927c3790e30af3935f02f579dc1d2b
DNA5769 <dennisthomas2002@gmail.com>
2022-04-23 21:46:19 +0530

    Merging is a pain

--- client/src/pages/admin.jsx
+++ client/src/pages/admin.jsx
@@ -30,31 +30,32 @@ const Admin = () => {
     }, [currentAccount]);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-5 font-inter">
+    <div className="flex flex-col items-center justify-center min-h-screen py-5 font-inter">
         <main className='flex flex-col items-center flex-1 w-full text-center px-44'>
-            <h3 className='text-3xl font-bold my-8'>Admin</h3>
+            { redirect && <Navigate to='/home' /> }
+            <h3 className='my-8 text-3xl font-bold'>Admin</h3>
 
-            <div className='grid grid-cols-3 gap-5 w-full'>
+            <div className='grid w-full grid-cols-3 gap-5'>
               {
                 allTips && allTips.map(tipoff =>
-                    <div className='w-full border rounded-md p-3 flex flex-col items-start hover:border-gray-500 font-inter bg-gray-50 hover:drop-shadow-md'>
-                        <div className='flex flex-row rounded-md w-full space-x-2 my-2'>
+                    <div className='flex flex-col items-start w-full p-3 border rounded-md hover:border-gray-500 font-inter bg-gray-50 hover:drop-shadow-md'>
+                        <div className='flex flex-row w-full my-2 space-x-2 rounded-md'>
                             <BiCurrentLocation size={25} className="w-1/12"/>
-                            <p className='text-left w-11/12'>{tipoff.location}</p>
+                            <p className='w-11/12 text-left'>{tipoff.location}</p>
                         </div>
                         
                         <div class="w-full border-t my-2"></div>
 
-                        <div className='flex flex-row rounded-md w-full space-x-2 my-2'>
+                        <div className='flex flex-row w-full my-2 space-x-2 rounded-md'>
                             <BiMessageDetail size={25} className="w-1/12"/>
-                            <p className='text-left w-11/12'>{tipoff.message}</p>
+                            <p className='w-11/12 text-left'>{tipoff.message}</p>
                         </div>
 
                         <div class="w-full my-2 border-t"></div> 
                         
-                        <div className='flex flex-row rounded-md w-full space-x-2 my-2'>
+                        <div className='flex flex-row w-full my-2 space-x-2 rounded-md'>
                             <AiOutlineEye size={25} className="w-1/12"/>
-                            <div className='w-11/12 flex flex-row'>
+                            <div className='flex flex-row w-11/12'>
                                 <AiOutlineFileJpg size={25} className="mt-1"/>
                                 <AiOutlineFileJpg size={25} className="mt-1 ml-2"/>
                                 <AiOutlineFileJpg size={25} className="mt-1 ml-2"/>
@@ -62,11 +63,11 @@ const Admin = () => {
                             </div>
                         </div>
 
-                        <div className='flex w-full item-center justify-center my-5'>
+                        <div className='flex justify-center w-full my-5 item-center'>
 
 
                             { parseFloat( tipoff.bounty.$numberDecimal ) === 0 &&
-                                <button onClick={()=>navigate('/transact')} className='flex flex-row items-center rounded-full bg-cyan-500 py-1 text-white hover:shadow-md hover:shadow-cyan-600 '>
+                                <button onClick={()=>navigate('/transact')} className='flex flex-row items-center py-1 text-white rounded-full bg-cyan-500 hover:shadow-md hover:shadow-cyan-600 '>
                                     <FaEthereum size={25} className="m-2" color='white'/> 
                                     <a className='mr-3 font-semibold'>Pay Now</a> 
                                 </button>
