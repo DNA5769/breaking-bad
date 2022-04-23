79c99824227a75f49af059f574f0e00c3ff5a349
Kushagra Sharma <skushagra2003@gmail.com>
2022-04-23 17:53:56 +0530

    auth almost done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import { Navigate } from 'react-router-dom'
+import * as API from '../../api/index'
 
 const Welcome = () => {
     const {connectWallet,currentAccount} = useContext(TransactionContext);
-    let admin = 0x52e1447a2c83d66216c6c0a4246fad6435a2de26;
+    
+    useEffect(() => {
+      if(currentAccount != "")
+        API.login(currentAccount).then(res => console.log(res.data));
+    }, [currentAccount])
+    
     return (<>
     <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
@@ -14,8 +20,6 @@ const Welcome = () => {
           </a>
         </h1>
         { console.log(currentAccount) }
-        { currentAccount && <Navigate to='/home' /> }
-        { currentAccount == admin && <Navigate to='/admin' /> }
         <button className="mt-3 text-2xl" onClick={connectWallet}>
           Connect Wallet
         </button>
@@ -23,5 +27,7 @@ const Welcome = () => {
 </>
 )};
 export default Welcome 
+// { currentAccount && <Navigate to='/home' /> }
+// { currentAccount == admin && <Navigate to='/admin' /> }
 // { currentAccount == admin ? <Navigate to='/admin' /> : <Navigate to='/home' /> }      
 // { console.log(admin) }
