fae35e68def59c3754a67802781eca7b1261fc11
DNA5769 <dennisthomas2002@gmail.com>
2022-04-24 05:47:59 +0530

    Hiding addresses in bounty page

--- client/src/pages/bounties.jsx
+++ client/src/pages/bounties.jsx
@@ -9,19 +9,19 @@ const Bounties = () =>{
         <div className="flex flex-col items-center justify-center min-h-screen py-5 font-inter">
             <main className='flex flex-col items-center flex-1 w-full text-center px-44'>
                 <h3 className='my-8 text-3xl font-bold'>Bounties</h3>
-                <table className='border w-full text-left font-inter'>
+                <table className='w-full text-left border font-inter'>
                 <thead>
                     <tr className='border'>
-                    <th className='text-gray-900 px-6 py-4 w-1/5'>Address To</th>
-                    <th className='text-gray-900 px-6 py-4 w-1/5'>Reward</th>
-                    <th className='text-gray-900 px-6 py-4 w-1/5'>Time</th>
+                    <th className='w-1/5 px-6 py-4 text-gray-900'>Address To</th>
+                    <th className='w-1/5 px-6 py-4 text-gray-900'>Reward</th>
+                    <th className='w-1/5 px-6 py-4 text-gray-900'>Time</th>
                     </tr>
                 </thead>
 
                 <tbody className='table-body'>
                     {
                         transactions.map(tran => <tr className="h-10">
-                            <td className="px-6">{tran.addressTo}</td>
+                            <td className="px-6">{tran.addressTo.substring(0, 5)}..............{tran.addressTo.split("").reverse().join("").substring(0, 5)}</td>
                             <td className="px-6">{tran.amount}</td>
                             <td className="px-6">{tran.timestamp}</td>
                         </tr>)
