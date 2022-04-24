bc0d6ca9b7b72133e291a6ff195350fb2361ffb8
DNA5769 <dennisthomas2002@gmail.com>
2022-04-24 06:13:33 +0530

    Improved homepage

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
+import { AiOutlineWallet } from 'react-icons/ai';
 import { Navigate } from 'react-router-dom'
 import * as API from '../../api/index'
 
@@ -19,17 +20,19 @@ const Welcome = () => {
     
     return (<>
     <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
         { redirectState === 1 && <Navigate to='/admin' /> }
         { redirectState === 2 && <Navigate to='/home' /> }
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Our App
-          </a>
+
+        <h1 className="text-6xl font-bold">
+          Breaking Bad
         </h1>
-        { console.log(currentAccount) }
-        <button className="mt-3 text-2xl" onClick={connectWallet}>
-          Connect Wallet
+        
+        <h2 className="m-3 text-3xl italic font-bold">
+          Earn Crypto by Reporting Drug Trafficking
+        </h2>
+        
+        <button className="flex items-center justify-between px-4 py-3 mt-5 text-lg text-white bg-blue-600 rounded-xl font-inter" onClick={connectWallet}>
+          <AiOutlineWallet className='mr-2' /> Connect Wallet
         </button>
     </main>
 </>
